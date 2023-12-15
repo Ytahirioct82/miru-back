36a0dfebbb81160f6b416616e7e899568b1c34a1
Younes Tahiri <you@example.com>
2023-12-15 17:47:28 -0500

    final

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const db = require("./db/db");
 initialize(passport);
 const app = express();
 app.set("db", db);
+
 app.use(
   cors({
     credentials: true,
@@ -20,6 +21,7 @@ app.use(
     ],
   })
 );
+
 app.use(express.json({ limit: "25mb" }));
 
 app.use(cookieParser());
@@ -40,6 +42,7 @@ app.use(
     },
   })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 
